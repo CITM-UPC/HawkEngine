7dd3ca6f9239d0372587285a2177bff56a0ad2b9
Jordopol <jordi.tur@estudiantat.upc.edu>
2024-10-30 23:08:11 +0100

    Rotation works, but the values make no sense. Mesh inspector working. Image inspector still not working

--- MyGameMaker/MyGameEditor/UIInspector.cpp
+++ MyGameMaker/MyGameEditor/UIInspector.cpp
@@ -6,6 +6,7 @@
 #include "..\MyGameEngine\Mesh.h"
 #include "Camera.h"
 #include "Input.h"
+#include "..\MyGameEngine\types.h"
 #include "..\MyGameEngine\MeshRendererComponent.h"
 #include "..\MyGameEngine\Mesh.h"
 #include "..\MyGameEngine\Image.h"
@@ -56,12 +57,12 @@ bool UIInspector::Draw()
 				ImGui::Separator();
 
 				glm::dvec3 currentPosition = transform->GetPosition();
-				glm::dvec3 currentRotation = transform->GetEulerAngles();
+				glm::dvec3 currentRotation = glm::radians(transform->GetEulerAngles());
 				glm::dvec3 currentScale = transform->GetScale();
 
 				float pos[3] = { static_cast<float>(currentPosition.x), static_cast<float>(currentPosition.y), static_cast<float>(currentPosition.z) };
 				// Ojo que hay que castear a grados creo
-				float rot[3] = { static_cast<float>(currentRotation.x), static_cast<float>(currentRotation.y), static_cast<float>(currentRotation.z) };
+				float rot[3] = { static_cast<float>(glm::degrees(currentRotation.x)), static_cast<float>(glm::degrees(currentRotation.y)), static_cast<float>(glm::degrees(currentRotation.z)) };
 				float sca[3] = { static_cast<float>(currentScale.x), static_cast<float>(currentScale.y), static_cast<float>(currentScale.z) };
 
 				if (ImGui::DragFloat3("Postition", pos, 0.1f))
@@ -73,21 +74,23 @@ bool UIInspector::Draw()
 
 				if (ImGui::DragFloat3("Rotation", rot, 0.1f))
 				{
-					glm::dvec3 newRotation = { rot[0], rot[1], rot[2] };
+					glm::dvec3 newRotation = glm::radians(glm::dvec3(rot[0], rot[1], rot[2]));
 					glm::dvec3 deltaRot = newRotation - currentRotation;
-					transform->Rotate(deltaRot.x, { 1, 0, 0 });
-					transform->Rotate(deltaRot.y, { 0, 1, 0 });
-					transform->Rotate(deltaRot.z, { 0, 0, 1 });
+
+					//float newX = deltaRot.x * DEGTORAD;
+					//float newY = deltaRot.y * DEGTORAD;
+					//float newZ = deltaRot.z * DEGTORAD;
+
+					transform->Rotate(deltaRot.x, glm::dvec3(1, 0, 0));
+					transform->Rotate(deltaRot.y, glm::dvec3(0, 1, 0));
+					transform->Rotate(deltaRot.z, glm::dvec3(0, 0, 1));
 				}
 
-				if (ImGui::DragFloat3("Scale", sca, 0.1f))
+				if (ImGui::DragFloat3("Scale", sca, 0.1f, 0.01f, 100.0f))
 				{
-					glm::dvec3 newScale = { sca[0], sca[1], sca[2] };
-					glm::dvec3 deltaScale = newScale - currentScale;
-					transform->Scale(deltaScale);
+					// Scale is just for visualization at the moment
 				}
 
-				// Methods to rotate and scale
 
 			}
 
@@ -100,6 +103,9 @@ bool UIInspector::Draw()
 				ImGui::Text("Mesh");
 				ImGui::Separator();
 
+				ImGui::Text("Vertices: %d", mesh->vertices().size());
+				ImGui::Text("Indices: %d", mesh->indices().size());
+
 			}
 
 			ImGui::Separator();
@@ -110,6 +116,7 @@ bool UIInspector::Draw()
 			{
 				ImGui::Text("Image");
 				ImGui::Separator();
+
 				ImGui::Text("Width: %d", image->width());
 				ImGui::Text("Heigth: %d", image->width());
 			}
